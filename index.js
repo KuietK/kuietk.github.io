ce0e6927b592599ed6931bfbf594d4aea31259d0
kuietk <kuietk@proton.me>
2024-06-28 14:44:02 -0500

    + random pick of array

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// CONSTANTS
+const letterS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+// theme toggle
 function darkmodE() {
     var element = document.body;
     element.classList.toggle("eyebleed")
 }
 
+// change text
 function changE() {
     var x = document.getElementById("helloDIV");
     if (x.innerHTML === "Hello World!") {
@@ -13,7 +19,20 @@ function changE() {
 
 }
 
+//function pasS() {
+//    var x = document.getElementById("passDIV");
+//    //wtf... just make it random
+//    for (let i in letterS) {
+//       console.log(i);
+// }
+// x.innerHTML = "5";
+//}
+
 function pasS() {
     var x = document.getElementById("passDIV");
-    x.innerHTML = "5";
+    // targets [passDIV]
+    var picK = letterS[Math.floor(Math.random() * letterS.length)];
+
+    x.innerHTML = picK
+
 }
